a43ebf97d3cf1935e3b833cd9c8fb51e657bae16
Aulyardha <aulyardhaanindita@gmail.com>
2022-12-15 11:00:43 +0700

    audy

---
+++ coba.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Uji Coba Penggunaan PHP di HTML</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam accusamus obcaecati nemo pariatur illo neque aperiam repudiandae quos tempora ad, explicabo velit perspiciatis eligendi nobis corporis mollitia cumque iure, fugiat error facere architecto, quo iste? Saepe repellendus voluptates neque! Suscipit, debitis impedit eos totam hic ad ipsum voluptate natus quia nostrum tempora praesentium reprehenderit ducimus architecto quae maxime rem eligendi error molestiae repellendus numquam a omnis sed! Repellendus alias molestiae consectetur nostrum nobis quos consequuntur, id possimus ipsam quae quia voluptas! Temporibus recusandae vitae impedit explicabo odio quibusdam exercitationem laudantium illum, amet, voluptatem, iure pariatur quidem? Excepturi molestiae nesciunt quasi.</p>
+
+    <!--masukkan php disini-->
+    <?php
+    $txt = "HELLO WORLD PHP";
+    echo "I love $txt!";
+    ?>
+
+    <?php
+    echo "Hello World! dari PHP";
+    ?>
+
+<?php
+$color = "red";
+echo "My car is " . $color . "<br>";
+echo "My house is " . $COLOR . "<br>";
+echo "My boat is " . $coLOR . "<br>";
+?>
+</body>
+</html>
